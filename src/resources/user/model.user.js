fe14e3c4d2a0a946ba884f03fb788a569db7395a
Aneesh S K <92055517+AneeshSaravuKarekad@users.noreply.github.com>
2022-03-08 18:27:33 -0500

    core: Setup User model

---
+++ src/resources/user/model.user.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+
+const { Schema, model } = mongoose;
+
+const userSchema = new Schema(
+  {
+    username: { type: String, required: true, trim: true },
+    email: { type: String, required: true, trim: true, unique: true },
+    password: { type: String, required: true, select: false },
+    birthDate: { type: String, required: true },
+    favourites: { type: Schema.Types.ObjectId, ref: 'Movie' },
+  },
+  { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    return next();
+  }
+  const hash = await bcrypt.hash(this.password, 10);
+  this.password = hash;
+  next();
+});
+
+userSchema.methods.comparePassword = async function (plainTextPassword) {
+  return await bcrypt.compare(plainTextPassword, this.password);
+};
+
+export default model('User', userSchema);
